3f0e938934b20891fa4809bbbe65247189ff60a1
ameysunu <ameysunu00719@gmail.com>
2022-09-15 16:15:30 +0100

    Initial Commit

---
+++ google-maps-platform/google_maps_platformApp.swift
@@ -0,0 +1,17 @@
+//
+//  google_maps_platformApp.swift
+//  google-maps-platform
+//
+//  Created by Amey Sunu on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct google_maps_platformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
