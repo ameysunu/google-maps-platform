dac0b0694e197cefc1418fb86a39bf8c583e9aa1
ameysunu <ameysunu00719@gmail.com>
2022-09-23 11:32:05 +0100

    initial commit

---
+++ google-maps-platform/AuthView.swift
@@ -0,0 +1,42 @@
+//
+//  AuthView.swift
+//  google-maps-platform
+//
+//  Created by Amey Sunu on 23/09/2022.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        ZStack(alignment: .leading){
+            Color(hex: "231651").ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Hi There!").font(.largeTitle).foregroundColor(.white)
+                Spacer()
+                Button(action:{}){
+                    Text("Create an account")
+                        .padding(15)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(UIColor.systemIndigo))
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            .frame(
+                  minWidth: 0,
+                  maxWidth: .infinity,
+                  minHeight: 0,
+                  maxHeight: .infinity,
+                  alignment: .topLeading
+                )
+            .padding(15)
+        }
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
