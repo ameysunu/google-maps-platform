e039cc214b0169d48271475e1107ff3b56367ab8
ameysunu <ameysunu00719@gmail.com>
2022-09-24 11:51:47 +0100

    build: registration view added

--- google-maps-platform/AuthView.swift
+++ google-maps-platform/AuthView.swift
@@ -12,6 +12,7 @@ struct AuthView: View {
     @State var username: String?
     @State var password: String?
     @StateObject var keyboardHandler = KeyboardHandler()
+    @State var createAccount: Bool = false
     
     var body: some View {
         ZStack(alignment: .leading){
@@ -20,33 +21,66 @@ struct AuthView: View {
                 Text("Welcome!").font(.largeTitle).foregroundColor(.white)
 
                 Spacer()
-
-                Image(uiImage: UIImage(named: "welcome")!)
-                    .resizable()
-                    .frame(width: 300, height: 300)
-                    .padding()
-                
-                                
-                MyTextField(username: username ?? "", textFieldText: "Email")
-                MyPasswordField(password: password ?? "", textFieldText: "Password")
-                
-                Button(action:{}){
-                    Text("Sign-in")
-                        .padding(15)
-                        .frame(maxWidth: .infinity)
-                        .background(Color(UIColor.systemIndigo))
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
-
-                
-                Button(action:{}){
-                    Text("Create an account")
-                        .padding(15)
-                        .frame(maxWidth: .infinity)
-                        .background(Color(UIColor.systemIndigo))
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
+                if createAccount{
+                    Image(uiImage: UIImage(named: "register")!)
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                        .padding()
+                    
+                    
+                    MyTextField(username: username ?? "", textFieldText: "Email")
+                    MyPasswordField(password: password ?? "", textFieldText: "Password")
+                    
+                    Button(action:{}){
+                        Text("Create an account")
+                            .padding(15)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(UIColor.systemIndigo))
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    
+                    
+                    Button(action:{
+                        self.createAccount = false
+                    }){
+                        Text("I already have an account")
+                            .padding(15)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(UIColor.systemIndigo))
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                } else {
+                    Image(uiImage: UIImage(named: "welcome")!)
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                        .padding()
+                    
+                    
+                    MyTextField(username: username ?? "", textFieldText: "Email")
+                    MyPasswordField(password: password ?? "", textFieldText: "Password")
+                    
+                    Button(action:{}){
+                        Text("Sign-in")
+                            .padding(15)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(UIColor.systemIndigo))
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    
+                    
+                    Button(action:{
+                        self.createAccount = true
+                    }){
+                        Text("Create an account")
+                            .padding(15)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(UIColor.systemIndigo))
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
                 }
             }
             .frame(
