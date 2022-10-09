318b4473cd73feec303cc8193233093f0dda8097
ameysunu <ameysunu00719@gmail.com>
2022-10-09 14:14:59 +0100

    build: navigation stack added

--- google-maps-platform/AuthView.swift
+++ google-maps-platform/AuthView.swift
@@ -16,175 +16,181 @@ struct AuthView: View {
     @StateObject var keyboardHandler = KeyboardHandler()
     @State var createAccount: Bool = false
     @State var showingPopup: Bool = false
+    @State var showHome: Bool = false
     
     var body: some View {
-        ZStack(alignment: .leading){
-            Color(hex: "231651").ignoresSafeArea()
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Welcome!").font(.largeTitle).foregroundColor(.white)
-
-                Spacer()
-                if createAccount{
-                    HStack{
-                        Spacer()
-                        Image(uiImage: UIImage(named: "register")!)
-                            .resizable()
-                            .frame(width: 300, height: 250)
+        NavigationStack {
+            ZStack(alignment: .leading){
+                Color(hex: "231651").ignoresSafeArea()
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Welcome!").font(.largeTitle).foregroundColor(.white)
+                    
+                    Spacer()
+                    if createAccount{
+                        HStack{
+                            Spacer()
+                            Image(uiImage: UIImage(named: "register")!)
+                                .resizable()
+                                .frame(width: 300, height: 250)
+                                .padding()
+                            Spacer()
+                        }
+                        
+                        TextField("", text: $username)
+                            .placeholder(when: username.isEmpty) {
+                                Text("Email").foregroundColor(.gray)
+                            }
+                            .foregroundColor(.white)
                             .padding()
-                        Spacer()
-                    }
-                    
-                    TextField("", text: $username)
-                        .placeholder(when: username.isEmpty) {
-                            Text("Email").foregroundColor(.gray)
-                    }
-                        .foregroundColor(.white)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(Color.white, lineWidth: 1.0))
-                        .padding(.bottom, 20)
-                        .autocapitalization(.none)
-                    
-                    SecureField("", text: $password)
-                        .placeholder(when: password.isEmpty) {
-                            Text("Password").foregroundColor(.gray)
-                    }
-                        .foregroundColor(.white)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(Color.white, lineWidth: 1.0))
-                        .padding(.bottom, 20)
-                        .autocapitalization(.none)
-                    
-                    SecureField("", text: $confirmPassword)
-                        .placeholder(when: confirmPassword.isEmpty) {
-                            Text("Confirm Password").foregroundColor(.gray)
-                    }
-                        .foregroundColor(.white)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(Color.white, lineWidth: 1.0))
-                        .padding(.bottom, 20)
-                        .autocapitalization(.none)
-                    
-                    Button(action:{
-                        if(password == confirmPassword){
-                            registerUser(email: username, password: password){
+                            .overlay(RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(Color.white, lineWidth: 1.0))
+                            .padding(.bottom, 20)
+                            .autocapitalization(.none)
+                        
+                        SecureField("", text: $password)
+                            .placeholder(when: password.isEmpty) {
+                                Text("Password").foregroundColor(.gray)
+                            }
+                            .foregroundColor(.white)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(Color.white, lineWidth: 1.0))
+                            .padding(.bottom, 20)
+                            .autocapitalization(.none)
+                        
+                        SecureField("", text: $confirmPassword)
+                            .placeholder(when: confirmPassword.isEmpty) {
+                                Text("Confirm Password").foregroundColor(.gray)
+                            }
+                            .foregroundColor(.white)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(Color.white, lineWidth: 1.0))
+                            .padding(.bottom, 20)
+                            .autocapitalization(.none)
+                        
+                        Button(action:{
+                            if(password == confirmPassword){
+                                registerUser(email: username, password: password){
+                                    (success) -> Void in
+                                    if success {
+                                        
+                                    } else {
+                                        self.showingPopup = true
+                                    }
+                                }
+                            } else {
+                                self.showingPopup = true
+                                errorMessage = "Entered passwords do not match"
+                            }
+                        }){
+                            Text("Create an account")
+                                .padding(15)
+                                .frame(maxWidth: .infinity)
+                                .background(Color(UIColor.systemIndigo))
+                                .foregroundColor(.white)
+                                .cornerRadius(8)
+                        }
+                        
+                        
+                        Button(action:{
+                            self.createAccount = false
+                        }){
+                            Text("I already have an account")
+                                .padding(15)
+                                .frame(maxWidth: .infinity)
+                                .background(Color(UIColor.systemIndigo))
+                                .foregroundColor(.white)
+                                .cornerRadius(8)
+                        }
+                    } else {
+                        HStack{
+                            Spacer()
+                            Image(uiImage: UIImage(named: "welcome")!)
+                                .resizable()
+                                .frame(width: 300, height: 300)
+                                .padding()
+                            Spacer()
+                        }
+                        
+                        TextField("", text: $username)
+                            .placeholder(when: username.isEmpty) {
+                                Text("Email").foregroundColor(.gray)
+                            }
+                            .foregroundColor(.white)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(Color.white, lineWidth: 1.0))
+                            .padding(.bottom, 20)
+                            .autocapitalization(.none)
+                        
+                        SecureField("", text: $password)
+                            .placeholder(when: password.isEmpty) {
+                                Text("Password").foregroundColor(.gray)
+                            }
+                            .foregroundColor(.white)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(Color.white, lineWidth: 1.0))
+                            .padding(.bottom, 20)
+                            .autocapitalization(.none)
+                        
+                        NavigationLink(destination: HomeView(), isActive: $showHome){
+                            
+                        }
+                        Button(action:{
+                            signInUser(email: username, password: password) {
                                 (success) -> Void in
                                 if success {
-
+                                    self.showHome = true
                                 } else {
                                     self.showingPopup = true
                                 }
                             }
-                        } else {
-                            self.showingPopup = true
-                            errorMessage = "Entered passwords do not match"
+                        }){
+                            Text("Sign-in")
+                                .padding(15)
+                                .frame(maxWidth: .infinity)
+                                .background(Color(UIColor.systemIndigo))
+                                .foregroundColor(.white)
+                                .cornerRadius(8)
                         }
-                    }){
-                        Text("Create an account")
-                            .padding(15)
-                            .frame(maxWidth: .infinity)
-                            .background(Color(UIColor.systemIndigo))
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                    
-                    
-                    Button(action:{
-                        self.createAccount = false
-                    }){
-                        Text("I already have an account")
-                            .padding(15)
-                            .frame(maxWidth: .infinity)
-                            .background(Color(UIColor.systemIndigo))
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                } else {
-                    HStack{
-                        Spacer()
-                        Image(uiImage: UIImage(named: "welcome")!)
-                            .resizable()
-                            .frame(width: 300, height: 300)
-                            .padding()
-                        Spacer()
-                    }
-                    
-                    TextField("", text: $username)
-                        .placeholder(when: username.isEmpty) {
-                            Text("Email").foregroundColor(.gray)
-                    }
-                        .foregroundColor(.white)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(Color.white, lineWidth: 1.0))
-                        .padding(.bottom, 20)
-                        .autocapitalization(.none)
-                    
-                    SecureField("", text: $password)
-                        .placeholder(when: password.isEmpty) {
-                            Text("Password").foregroundColor(.gray)
-                    }
-                        .foregroundColor(.white)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(Color.white, lineWidth: 1.0))
-                        .padding(.bottom, 20)
-                        .autocapitalization(.none)
-                    
-                    Button(action:{
-                        signInUser(email: username, password: password) {
-                            (success) -> Void in
-                            if success {
-                                
-                            } else {
-                                self.showingPopup = true
-                            }
+                        
+                        
+                        Button(action:{
+                            self.createAccount = true
+                        }){
+                            Text("Create an account")
+                                .padding(15)
+                                .frame(maxWidth: .infinity)
+                                .background(Color(UIColor.systemIndigo))
+                                .foregroundColor(.white)
+                                .cornerRadius(8)
                         }
-                    }){
-                        Text("Sign-in")
-                            .padding(15)
-                            .frame(maxWidth: .infinity)
-                            .background(Color(UIColor.systemIndigo))
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                    
-                    
-                    Button(action:{
-                        self.createAccount = true
-                    }){
-                        Text("Create an account")
-                            .padding(15)
-                            .frame(maxWidth: .infinity)
-                            .background(Color(UIColor.systemIndigo))
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
                     }
                 }
-            }
-            .frame(
-                  minWidth: 0,
-                  maxWidth: .infinity,
-                  minHeight: 0,
-                  maxHeight: .infinity,
-                  alignment: .topLeading
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    maxHeight: .infinity,
+                    alignment: .topLeading
                 )
-            .padding(15)
-        }
-        .popup(isPresented: $showingPopup, type: .floater(verticalPadding: 10.0, useSafeAreaInset: true), position: .top, autohideIn: 2, dragToDismiss: true) {
-            VStack{
-                Text("Error: \(errorMessage)")
-                    .font(.title3)
-                    .padding(10)
+                .padding(15)
             }
+            .popup(isPresented: $showingPopup, type: .floater(verticalPadding: 10.0, useSafeAreaInset: true), position: .top, autohideIn: 2, dragToDismiss: true) {
+                VStack{
+                    Text("Error: \(errorMessage)")
+                        .font(.title3)
+                        .padding(10)
+                }
                 .font(.body)
                 .foregroundColor(.white)
                 .frame(maxWidth: .infinity, minHeight: 100.0)
                 .background(Color(hex: "ff4d4d"))
                 .cornerRadius(20)
                 .padding(15)
+            }
         }
     }
 }
---
+++ google-maps-platform/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  google-maps-platform
+//
+//  Created by Amey Sunu on 09/10/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
